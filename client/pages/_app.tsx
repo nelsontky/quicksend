a46331fe956234a10aaf6c1349a7011a4d434d5f
nelsontky <nelson@ccb.wtf>
2021-01-20 00:44:51 +0800

    Setup material ui

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,6 +1,32 @@
-import '../styles/global.css'
-import { AppProps } from 'next/app'
+import * as React from "react";
+import Head from "next/head";
+import { AppProps } from "next/app";
+import { CacheProvider } from "@emotion/react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import createCache from "@emotion/cache";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export const cache = createCache({ key: "css", prepend: true });
+
+export default function MyApp(props: AppProps) {
+  const { Component, pageProps } = props;
+
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement!.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <CacheProvider value={cache}>
+      <Head>
+        <title>My page</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+      <CssBaseline />
+      <Component {...pageProps} />
+    </CacheProvider>
+  );
 }

2d094cc209e796f01f963239efa59a9ac811bfbc
nelsontky <nelson@ccb.wtf>
2021-01-28 03:09:16 +0800

    Done up multipart upload backend

--- server/src/files/files.controller.ts
+++ server/src/files/files.controller.ts
@@ -1,34 +1,21 @@
-import { Controller, Get, Post, Body, Put, Param, Delete } from '@nestjs/common';
-import { FilesService } from './files.service';
-import { CreateFileDto } from './dto/create-file.dto';
-import { UpdateFileDto } from './dto/update-file.dto';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Put,
+  Param,
+  Delete,
+} from "@nestjs/common";
+import { FilesService } from "./files.service";
+import { CreateFileDto } from "./dto/create-file.dto";
 
-@Controller('files')
+@Controller("files")
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
 
   @Post()
-  create(@Body() createFileDto: CreateFileDto) {
-    return this.filesService.create(createFileDto);
+  async create(@Body() createFileDto: CreateFileDto) {
+    await this.filesService.create(createFileDto);
   }
-
-  // @Get()
-  // findAll() {
-  //   return this.filesService.findAll();
-  // }
-
-  // @Get(':id')
-  // findOne(@Param('id') id: string) {
-  //   return this.filesService.checkFileExists(id);
-  // }
-
-  // @Put(':id')
-  // update(@Param('id') id: string, @Body() updateFileDto: UpdateFileDto) {
-  //   return this.filesService.update(+id, updateFileDto);
-  // }
-
-  // @Delete(':id')
-  // remove(@Param('id') id: string) {
-  //   return this.filesService.remove(+id);
-  // }
 }
--- server/src/uploads/uploads.controller.ts
+++ server/src/uploads/uploads.controller.ts
@@ -1,10 +1,14 @@
 import { Body, Controller, Get, Post, Query } from "@nestjs/common";
+import { FilesService } from "src/files/files.service";
 import { CreateFileDto } from "./dto/create-file.dto";
 import { UploadsService } from "./uploads.service";
 
 @Controller("uploads")
 export class UploadsController {
-  constructor(private readonly uploadsService: UploadsService) {}
+  constructor(
+    private readonly uploadsService: UploadsService,
+    private readonly filesService: FilesService
+  ) {}
 
   @Get()
   getUploadUrls(
@@ -14,8 +18,14 @@ export class UploadsController {
     return this.uploadsService.getUploadUrls(fileSize, fileType);
   }
 
-  @Post("/complete")
-  completeUpload(@Body() body: CreateFileDto) {
-    this.uploadsService.completeUpload(body);
+  @Post("complete")
+  async completeUpload(@Body() body: CreateFileDto) {
+    await this.uploadsService.completeUpload(body);
+    return await this.filesService.create({
+      id: body.id,
+      name: body.name,
+      size: body.size,
+      type: body.type,
+    });
   }
 }

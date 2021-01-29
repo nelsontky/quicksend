aa46628477fed8fff2eabb9b3811f91f41c1bd92
nelsontky <nelson@ccb.wtf>
2021-01-29 12:24:28 +0800

    Copy component

--- client/components/InputCopy.tsx
+++ client/components/InputCopy.tsx
@@ -3,7 +3,8 @@ import { createStyles, Theme, makeStyles } from "@material-ui/core/styles";
 import { Grid, IconButton } from "@material-ui/core";
 import { FileCopyTwoTone as FileCopyIcon } from "@material-ui/icons";
 
-import Button from "./Button";
+import { useAppDispatch } from "../store";
+import { alert } from "../slices/snackbarsSlice";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -12,7 +13,7 @@ const useStyles = makeStyles((theme: Theme) =>
       width: "100%",
     },
     icon: {
-      color: theme.palette.grey[500],
+      color: theme.palette.grey[200],
     },
     input: {
       borderTopRightRadius: "0",
@@ -42,18 +43,32 @@ export default function InputCopy(
   >
 ) {
   const classes = useStyles();
+  const inputRef = React.useRef(null);
+
+  const onCopy = () => {
+    if (inputRef.current) {
+      inputRef.current.select();
+      document.execCommand("copy");
+      dispatch(
+        alert({ message: "Link copied to clipboard!", severity: "success" })
+      );
+    }
+  };
+
+  const dispatch = useAppDispatch();
 
   return (
     <Grid container alignItems="center">
       <Grid item xs={10}>
         <input
+          {...props}
+          ref={inputRef}
           className={classes.input}
           type="text"
-          value="https://github.com/zenorocha/clipboard.js.git"
         ></input>
       </Grid>
       <Grid item xs={2}>
-        <IconButton>
+        <IconButton onClick={onCopy}>
           <FileCopyIcon className={classes.icon} />
         </IconButton>
       </Grid>

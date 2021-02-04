e1b5f73f47488fead2127ecf42929423a85b8a0f
nelsontky <nelson@ccb.wtf>
2021-02-04 17:56:48 +0000

    Start work on database admin

---
+++ admin/src/index.ts
@@ -0,0 +1,23 @@
+import "reflect-metadata";
+import { createConnection, getRepository } from "typeorm";
+
+import { File } from "./entity/file.entity";
+
+createConnection()
+  .then(async (connection) => {
+    const fileRepository = getRepository(File);
+    const files = await fileRepository.find({ select: ["id", "name"] });
+    console.log(files);
+    // console.log("Inserting a new user into the database...");
+    // const user = new User();
+    // user.firstName = "Timber";
+    // user.lastName = "Saw";
+    // user.age = 25;
+    // await connection.manager.save(user);
+    // console.log("Saved a new user with id: " + user.id);
+    // console.log("Loading users from the database...");
+    // const users = await connection.manager.find(User);
+    // console.log("Loaded users: ", users);
+    // console.log("Here you can setup and run express/koa/any other framework.");
+  })
+  .catch((error) => console.log(error));

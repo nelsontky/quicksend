a6a8101397ade487a866c7bc523aca8c242337c5
nelsontky <nelson@ccb.wtf>
2021-02-05 16:50:07 +0000

    Add meta tags to index

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -12,6 +12,26 @@ export default function Index() {
     <>
       <Head>
         <title>Quick Send</title>
+        <meta name="title" content="Quick Send" />
+        <meta
+          name="description"
+          content="Send your files quickly! Upload up to 20GB per file for free on the fastest file hosting today!"
+        />
+        {/* <!-- Open Graph / Facebook --> */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://quicksend.io/" />
+        <meta property="og:title" content="Quick Send" />
+        <meta
+          property="og:description"
+          content="Send your files quickly! Upload up to 20GB per file for free on the fastest file hosting today!"
+        />
+        {/* <!-- Twitter --> */}
+        <meta property="twitter:url" content="https://quicksend.io/" />
+        <meta property="twitter:title" content="Quick Send" />
+        <meta
+          property="twitter:description"
+          content="Send your files quickly! Upload up to 20GB per file for free on the fastest file hosting today!"
+        />
       </Head>
       <ProductHero />
       {/* <ProductValues /> */}

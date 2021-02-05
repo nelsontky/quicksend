b1e2392e874a8f84710784f344b607940c942f59
nelsontky <nelson@ccb.wtf>
2021-02-05 11:05:51 +0000

    Add initial cron job

--- admin/src/index.ts
+++ admin/src/index.ts
@@ -2,25 +2,32 @@ import "reflect-metadata";
 import { createConnection, getRepository } from "typeorm";
 import { SitemapStream, streamToPromise } from "sitemap";
 import { writeFileSync } from "fs";
+import * as schedule from "node-schedule";
 
 import { File } from "../../server/src/files/entities/file.entity";
 
 const { Readable } = require("stream");
 
+async function genSitemap() {
+  console.log("Generating new site map");
+  const fileRepository = getRepository(File);
+  const files = await fileRepository.find({ select: ["id"] });
+
+  // An array with your links
+  const links = files.map((file) => ({ url: `/${file.id}` }));
+
+  // Create a stream to write to
+  const stream = new SitemapStream({ hostname: "https://quicksend.cloud" });
+
+  // Return a promise that resolves with your XML string
+  return streamToPromise(Readable.from(links).pipe(stream)).then((data) => {
+    writeFileSync("../client/public/sitemaps/sitemap.xml", data.toString());
+  });
+}
+
 createConnection()
-  .then(async () => {
-    const fileRepository = getRepository(File);
-    const files = await fileRepository.find({ select: ["id"] });
-
-    // An array with your links
-    const links = files.map((file) => ({ url: `/${file.id}` }));
-
-    // Create a stream to write to
-    const stream = new SitemapStream({ hostname: "https://quicksend.cloud" });
-
-    // Return a promise that resolves with your XML string
-    return streamToPromise(Readable.from(links).pipe(stream)).then((data) => {
-      writeFileSync("../client/public/sitemaps/sitemap.xml", data.toString());
-    });
+  .then(() => {
+    console.log("cron job started");
+    schedule.scheduleJob("*/1 * * * *", genSitemap);
   })
   .catch((error) => console.log(error));

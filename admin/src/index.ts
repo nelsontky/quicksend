07f3268cd0a010120117c8923e4eeb3cd578dd2a
nelsontky <nelson@ccb.wtf>
2021-02-05 09:39:30 +0000

    Move sitemap to next folder

--- admin/src/index.ts
+++ admin/src/index.ts
@@ -1,15 +1,16 @@
 import "reflect-metadata";
 import { createConnection, getRepository } from "typeorm";
 import { SitemapStream, streamToPromise } from "sitemap";
-import { File } from "../../server/src/files/entities/file.entity";
 import { writeFileSync } from "fs";
+
+import { File } from "../../server/src/files/entities/file.entity";
 
 const { Readable } = require("stream");
 
 createConnection()
-  .then(async (connection) => {
+  .then(async () => {
     const fileRepository = getRepository(File);
-    const files = await fileRepository.find({ select: ["id", "name"] });
+    const files = await fileRepository.find({ select: ["id"] });
 
     // An array with your links
     const links = files.map((file) => ({ url: `/${file.id}` }));
@@ -19,7 +20,7 @@ createConnection()
 
     // Return a promise that resolves with your XML string
     return streamToPromise(Readable.from(links).pipe(stream)).then((data) => {
-      writeFileSync("./src/sitemap.xml", data.toString());
+      writeFileSync("../client/public/sitemaps/sitemap.xml", data.toString());
     });
   })
   .catch((error) => console.log(error));
